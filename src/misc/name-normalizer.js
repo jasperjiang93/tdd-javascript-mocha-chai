7d30cfc207c0fef1e4f4f9022475b3c611104193
Jasper <gcnvq@monsanto.com>
2018-12-03 14:12:33 -0600

    origin

--- src/misc/name-normalizer.js
+++ src/misc/name-normalizer.js
@@ -1,15 +1,5 @@
-const countOfChar = (s, c) => (s.match(new RegExp(c, 'g'))||[]).length
-const isMononym = nameParts => (nameParts.length === 1)
-const lastName = name => (parts(name)[1])
-const firstName = name => (parts(name)[0])
-const parts = name => name.split(' ')
+const countOfChar = (s, c) => (s.match(new RegExp(c, 'g'))||[]).length;
 
-export const normalize = name => {
-  if(name === '') return ''
-  const nameParts = name.trim().split(' ')
-  if(isMononym(nameParts)) return name
-  if(nameParts.length === 2)
-    return `${lastName()}, ${nameParts[0]}`
-  // if(nameParts.length === 3)
-
+export const normalize = _ => {
+  return 'wha?'
 }
